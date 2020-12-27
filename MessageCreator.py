ef78828b5ffe03f281acff2dc51db422bfe87566
Kuba Łukaszczyk <qba.lukaszczyk@gmail.com>
2020-12-27 19:59:42 +0100

    GUI first update
    Added first GUI window and added message creator file for further development

---
+++ MessageCreator.py
@@ -0,0 +1,2 @@
+# class MessageCreator:
+#     def prepareMessageContent(self,)
---
+++ main.py
@@ -0,0 +1,52 @@
+from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QListWidget, QPushButton, QGroupBox, QGridLayout, QDialog)
+
+
+class WidgetGallery(QDialog):
+    def __init__(self, parent=None):
+        super(WidgetGallery, self).__init__(parent)
+
+        self.createVotingList()
+        self.createActionList()
+
+        mainLayout = QGridLayout()
+        mainLayout.addWidget(self.leftBox, 0, 0)
+        mainLayout.addWidget(self.rightBox, 0, 1)
+        mainLayout.setRowStretch(1, 1)
+        mainLayout.setRowStretch(2, 1)
+        mainLayout.setColumnStretch(0, 1)
+        mainLayout.setColumnStretch(1, 1)
+        self.setLayout(mainLayout)
+
+        self.setWindowTitle("votePy")
+
+    def createVotingList(self):
+        self.leftBox = QGroupBox("Ongoing votings")
+        listWidget = QListWidget()
+
+        layout = QVBoxLayout()
+        layout.addWidget(listWidget)
+        layout.addStretch(1)
+        self.leftBox.setLayout(layout)    
+
+    def createActionList(self):
+        self.rightBox = QGroupBox("Actions")
+
+        firstButton = QPushButton("New voting")
+        firstButton.setDefault(True)
+
+        secondButton = QPushButton("About")
+        layout = QVBoxLayout()
+        layout.addWidget(firstButton)
+        layout.addWidget(secondButton)
+        layout.addStretch(1)
+        self.rightBox.setLayout(layout)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    guiApp = QApplication(sys.argv)
+    gallery = WidgetGallery()
+    gallery.show()
+    sys.exit(guiApp.exec_()) 
