d349a0a71120d9930565e1338a5e46750a90b5be
Kuba Łukaszczyk <qba.lukaszczyk@gmail.com>
2021-01-11 20:53:39 +0100

    Update RegistrationHandler.py

--- RegistrationHandler.py
+++ RegistrationHandler.py
@@ -8,13 +8,20 @@ class RegistrationHandler(QObject):
     finished = pyqtSignal()
     hostsToNotify = []
     context = zmq.Context()
+    hostsToRemove = []
     socket = context.socket(zmq.REQ)
+    socket.RCVTIMEO = 3000
 
     def __init__(self):
         super(RegistrationHandler, self).__init__()
 
     def updateHostsToNotify(self, availableHosts):
         self.hostsToNotify = availableHosts
+
+    def resetSocket(self):
+        self.socket.close()
+        self.socket = self.context.socket(zmq.REQ)
+        self.socket.RCVTIMEO = 3000
  
     def runRegistration(self):
         print("Running registration")
@@ -24,9 +31,14 @@ class RegistrationHandler(QObject):
         if len(self.hostsToNotify) != 0:
             for host in self.hostsToNotify:
                 self.socket.connect("tcp://%s:6968" % host)
-            print("Sending registration message")
-            self.socket.send_string(localhostIpAddress)
-            message = self.socket.recv_string()
-            print("Received registration response: ", message)
+                print("Sending registration message")
+                self.socket.send_string(localhostIpAddress)
+                try:
+                    message = self.socket.recv_string()
+                    print("Received registration response: ", message)
+                except:
+                    print("Registration for host %s not possible" % host)
+                    self.hostsToRemove.append(host)
+                self.resetSocket()
         self.socket.close()
         self.finished.emit()
